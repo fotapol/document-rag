07d221b56baa520bab898f2c655631092910ebfb
Kirill Krupin <krupinkd@gmail.com>
2026-07-16 15:06:50 +0200

    feat: add dataset preparation contracts

---
+++ src/document_rag/datasets/__init__.py
@@ -0,0 +1,17 @@
+from document_rag.datasets.models import (
+    DatasetExample,
+    DatasetName,
+    DatasetSplit,
+    ReasoningStep,
+    ReferenceAnswer,
+    SupportingFact,
+)
+
+__all__ = [
+    "DatasetExample",
+    "DatasetName",
+    "DatasetSplit",
+    "ReasoningStep",
+    "ReferenceAnswer",
+    "SupportingFact",
+]
