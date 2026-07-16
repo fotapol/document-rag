d9f8128a6e6620573dcc6819033c1125542abf99
Kirill Krupin <krupinkd@gmail.com>
2026-07-16 11:02:01 +0200

    feat: add question domain model

--- src/document_rag/domain/__init__.py
+++ src/document_rag/domain/__init__.py
@@ -1,13 +1,18 @@
+from document_rag.domain.base import BaseDomainModel
 from document_rag.domain.documents import (
     Document,
     DocumentElement,
     DocumentElementType,
     TableCoordinates,
 )
+from document_rag.domain.questions import Question, QuestionType
 
 __all__ = [
+    "BaseDomainModel",
     "Document",
     "DocumentElement",
     "DocumentElementType",
+    "Question",
+    "QuestionType",
     "TableCoordinates",
 ]
